f0a8179cbf64c899c3d0a38767f048b625381425
Seolseol333 <hiseol0125@gmail.com>
2024-07-24 22:10:35 +0900

    Add components

---
+++ src/components/TermsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TermsPage = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>약관 동의</h1>
+      <p>여기에 약관 내용을 입력하세요...</p>
+      {/* 필요한 다른 내용 추가 */}
+    </div>
+  );
+};
+
+export default TermsPage;
